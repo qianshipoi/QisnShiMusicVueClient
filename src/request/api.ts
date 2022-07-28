6d91df3e2e6cb00b5b0a3eea218f9bd360859455
qianshi <2410128587@qq.com>
2022-07-28 17:05:04 +0800

    implement search page.

--- src/request/api.ts
+++ src/request/api.ts
@@ -4,7 +4,7 @@ class api {
   public static playlist = {
     hot: () => Request.get('/playlist/hot'),
     topPlaylistHighquality: (cat: string, limit: number = 50, before: number | undefined) => Request.get('/top/playlist/highquality', { cat, limit, before }),
-    detail: (id: number) => Request.get('/playlist/detail', { id })
+    detail: (id: number) => Request.get('/playlist/detail', { id }),
   }
 }
 
--- src/views/home/Search.vue
+++ src/views/home/Search.vue
@@ -1,12 +1,36 @@
 <template>
   <div>
-    <search-box></search-box>
+    <n-input-group>
+      <n-input placeholder="input content to search"
+               v-model:value="content"
+               :style="{ width: '30%', textAlign:'left'}"
+               autofocus
+               @keypress.enter="search" />
+      <n-button type="primary"
+                @click="search"
+                ghost>
+        搜索
+      </n-button>
+    </n-input-group>
   </div>
 </template>
 
 <script setup lang="ts">
-import SearchBox from '@/components/SearchBox.vue'
+import { useMessage } from 'naive-ui'
+import { ref, watchEffect } from 'vue-demi'
+import { useRoute, useRouter } from 'vue-router'
 
+const router = useRouter()
+const route = useRoute()
+const message = useMessage()
+const content = ref<string>()
+watchEffect(() => {
+  content.value = route.query['content']?.toString()
+})
+
+const search = () => {
+  message.info('search content:' + content.value)
+}
 </script>
 
 <style scoped>
