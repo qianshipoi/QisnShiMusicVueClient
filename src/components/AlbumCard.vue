379ad0c31a592181176be2bf64d7371c96d60332
未来 栗山 <2410128587@qq.com>
2022-07-28 20:26:36 +0800

    added album card component.

---
+++ src/components/AlbumCard.vue
@@ -0,0 +1,47 @@
+<template>
+  <n-card class="album-card"
+          v-if="!album">
+    <template #cover>
+      <n-skeleton width="196px"
+                  height="196px"
+                  :sharp="false" />
+      <n-skeleton text
+                  style="margin:1rem"
+                  width="160px"
+                  height="28px" />
+    </template>
+  </n-card>
+  <n-card v-else
+          class="album-card"
+          :title="album.name">
+    <template #cover>
+      <n-image :src="album.picUrl"
+               :alt="album.name"
+               preview-disabled
+               style="width:14rem;height:14rem" />
+    </template>
+  </n-card>
+</template>
+
+<script setup lang="ts">
+import { Album } from '@/typings/neteasecloudmusicapi'
+import { useThemeVars } from 'naive-ui'
+
+defineProps<{
+  album?: Album
+}>()
+
+const themeVars = useThemeVars()
+</script>
+
+<style lang="scss" scoped>
+.album-card {
+  cursor: pointer;
+  max-width: 14rem;
+  transition: all ease-in-out 0.2s;
+  &:hover {
+    transform: perspective(1px) scale(1.01);
+    box-shadow: 0 2px 10px v-bind('themeVars.primaryColor');
+  }
+}
+</style>
--- src/views/home/Search.vue
+++ src/views/home/Search.vue
@@ -12,40 +12,63 @@
         搜索
       </n-button>
     </n-input-group>
-    <n-text>{{albums}}</n-text>
+    <n-space v-if="isBusy"
+             justify="space-between">
+      <album-card v-for="item in 10"
+                  :key="item"></album-card>
+    </n-space>
+    <n-space v-else
+             justify="space-between">
+      <album-card :album="album"
+                  v-for="album in albums"
+                  :key="album.id"></album-card>
+    </n-space>
+
   </div>
 </template>
 
 <script setup lang="ts">
 import { useMessage, useThemeVars } from 'naive-ui'
-import { ref, watchEffect } from 'vue-demi'
-import { useRoute, useRouter } from 'vue-router'
+import { onMounted, ref, watchEffect } from 'vue-demi'
+import { useRoute } from 'vue-router'
 import { api } from '@/request/api'
 import { Album } from '@/typings/neteasecloudmusicapi'
+import AlbumCard from '@/components/AlbumCard.vue'
 
-const router = useRouter()
 const route = useRoute()
 const message = useMessage()
 const themeVars = useThemeVars()
 const content = ref<string>()
+const isBusy = ref(false)
 watchEffect(() => {
   content.value = route.query['content']?.toString()
+})
+
+onMounted(() => {
+  if (route.query['content']) {
+    search()
+  }
 })
 
 const albums = ref<Array<Album>>()
 
 const search = async () => {
   if (content.value !== undefined) {
-    const { status, data } = await api.playlist.search(
-      content.value,
-      undefined,
-      0,
-      10
-    )
-    if (status === 200 && data.code === 200) {
-      albums.value = data.result.albums
-    } else {
-      message.error(data.msg)
+    isBusy.value = true
+    try {
+      const { status, data } = await api.playlist.search(
+        content.value,
+        undefined,
+        0,
+        10
+      )
+      if (status === 200 && data.code === 200) {
+        albums.value = data.result.albums
+      } else {
+        message.error(data.msg)
+      }
+    } finally {
+      isBusy.value = false
     }
   }
 }
